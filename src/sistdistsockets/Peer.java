05bde22263ebb577648b42d9386c7f68530fd972
Davi Pereira Neto <davineto@alunos.utfpr.edu.br>
2017-09-05 17:16:29 -0300

    javadoc da classe Peer criado

--- src/sistdistsockets/Peer.java
+++ src/sistdistsockets/Peer.java
@@ -10,8 +10,15 @@ import java.net.*;
 import java.util.*;
 
 /**
- *
+ * A classe Peer encapsula as informações de um nó da arquitetura Peer-to-Peer de redes
+ * de computadores, que funciona tanto como cliente quanto servidor, permitindo
+ * compartilhamento de serviços e dados sem a necessidade de um servidor central.<p>
+ * Nessa implementação, um dos peers funciona como um indexador, que é responsável por
+ * fornecer as chaves públicas para comunicação unicast entre os pares, e fornecer informações
+ * sobre os produtos disponíveis para venda em cada peer.</p>
  * @author davi
+ * @see MulticastSocket
+ * @see MultipeerReceiveThread
  */
 
 public class Peer {
@@ -26,7 +33,15 @@ public class Peer {
     lista dos peers ativos no grupo, utilizada quando o indexador cai, para que o peer compare seu iD com o iD
     dos outros peers, e se o menor iD for o dele, invocara o metodo para ser elegido como indexador
     */
+    
     private ArrayList<Integer> peersOnGroup;
+    
+    /**
+     * Cria um peer com id, porta e grupo multicast especificados.
+     * @param id o identificador do par
+     * @param port a porta para comunicação unicast
+     * @param group o grupo Multicast ao que o par vai pertencer
+     */
     
     public Peer(int id, int port, String group) {
         this.iD = id;
@@ -42,7 +57,12 @@ public class Peer {
         verifyAliveIndexerTimer.scheduleAtFixedRate(vai, 5000, 5000);
     }
     
-    //metodo para enviar uma mensagem para o grupo Multicast
+    
+    
+    /**
+     * Envia uma mensagem para o grupo Multicast.
+     * @param message a mensagem a ser enviada
+     */
     public void send(String message) {
         MulticastSocket s = null;
         try {
@@ -69,7 +89,9 @@ public class Peer {
         }
     }
     
-    //define o peer como indexador
+    /**
+     * Define este par como o indexador.
+     */
     public void beTheIndexer() {
         System.out.println("Eu, peer " + iD + " vou ser o indexador!!");
         //envia a mensagem de novo indexador para o grupo Multicast, para os outros peers informarem seus dados
@@ -85,38 +107,75 @@ public class Peer {
         informAliveIndexerTimer.schedule(informAliveIndexerTimerTask, 0, 5000);
     }
     
+    /**
+     * Adiciona o iD especificado a uma lista de iDs utilizada para eleger um novo indexador
+     * quando o atual cair.
+     * @param iD o iD a ser adicionado na lista.
+     */
     public void putPeersOnGroup(int iD) {
         peersOnGroup.add(iD);
     }
 
+    /**
+     * Retorna o iD do par.
+     * @return um valor <i>int</i> que representa o identificador do par.
+     */
     public int getID() {
         return iD;
     }
-
+    
+    /**
+     * Retorna a porta em que o par está escutando na comunicação Unicast.
+     * @return um valor <i>int<i> que representa a porta do par para comunicação Unicast.
+     */
     public int getPort() {
         return port;
     }
 
+    /**
+     * Retorna o grupo Multicast em que o par está inserido.
+     * @return uma <i>String</i> que representa o IP do grupo Multicast
+     */
     public String getGroup() {
         return group;
     }
 
+    /**
+     * Retorna a porta em que o indexador está escutando na comunicação Unicast.
+     * @return um valor <i>int</i> que representa a porta do par indexador para comunicação Unicast
+     */
     public int getIndexerPort() {
         return indexerPort;
     }
 
+    /**
+     * Retorna a lista de iDs dos peers do grupo Multicast, que é preenchida quando o indexador cai.
+     * @return um <i>ArrayList</i> de inteiros contendo os iDs dos peers do grupo Multicast
+     */
     public ArrayList<Integer> getPeersOnGroup() {
         return peersOnGroup;
     }
 
+    /**
+     * Teste se o indexador está vivo.
+     * @return um <i>boolean</i> indicando se o indexador está vivo ou não
+     */
     public boolean isIndexerOn() {
         return indexerOn;
     }
 
+    /**
+     * Define se o indexador está vivo.
+     * @param indexerOn um <i>boolean</i> indicando se o indexador está vivo ou não
+     */
     public void setIndexerOn(boolean indexerOn) {
         this.indexerOn = indexerOn;
     }
 
+    /**
+     * Seta o valor da porta do indexador.
+     * @param indexerPort um <i>int</i> indicando o valor da porta do indexador
+     */
     public void setIndexerPort(int indexerPort) {
         this.indexerPort = indexerPort;
     }
